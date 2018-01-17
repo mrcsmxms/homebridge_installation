d18c20eadc27a8f359501c5843b67e3d70cce9d2
mrcsmxms <marcus.assenheimer@gmx.de>
2018-01-17 23:34:29 +0100

    abc

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
-wget https://nodejs.org/dist/v4.3.2/node-v4.3.2-linux-armv6l.tar.gz
-tar -xvf node-v4.3.2-linux-armv6l.tar.gz
-cd node-v4.3.2-linux-armv6l
-sudo cp -R * /usr/local/
-cd
-cd
-sudo apt-get install libavahi-compat-libdnssd-dev
+sudo ap-get update
+sudo apt-get install npm -y
+sudo npm -g n
+sudo n latest
+sudo apt-get install libavahi-compat-libdnssd-dev -y
 sudo npm install -g --unsafe-perm homebridge
+sudo homebridge &
+sleep 9
+sudo killall homebridge
+cd .homebridge/
+sudo wget https://raw.githubusercontent.com/mrcsmxms/homebridge_installation/master/config.json
+cd
+

8b99bd7f6fc9b2a43de983096bbc8cebbce0557c
mrcsmxms <marcus.assenheimer@gmx.de>
2018-01-18 00:44:41 +0100

    abc

--- installHB.sh
+++ installHB.sh
@@ -1,13 +1,35 @@
+echo "
+  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+    ░░░░▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒░░▒▒▒▒▒▒░░▒▒▒▒▒▒░░░░░░
+  ░░░░░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░░░░░▒▒░░░░░░░░
+    ░░░░▒▒░░▒▒░░▒▒░░▒▒▒▒▒▒░░▒▒░░░░░░▒▒▒▒▒▒░░░░░░
+  ░░░░░░▒▒░░▒▒░░▒▒░░▒▒▒▒░░░░▒▒░░░░░░░░░░▒▒░░░░
+    ░░░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒▒▒▒▒░░▒▒▒▒▒▒░░░░░░
+  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+    ░░▒▒▒▒▒▒▒▒▒▒░░▒▒░░▒▒░░▒▒▒▒▒▒▒▒▒▒░░▒▒▒▒▒▒░░░░
+  ░░░░▒▒░░▒▒░░▒▒░░░▒▒▒▒░░░▒▒░░▒▒░░▒▒░░▒▒░░░░░░
+    ░░▒▒░░▒▒░░▒▒░░░░▒▒░░░░▒▒░░▒▒░░▒▒░░▒▒▒▒▒▒░░░░
+  ░░░░▒▒░░▒▒░░▒▒░░░▒▒▒▒░░░▒▒░░▒▒░░▒▒░░░░░░▒▒░░
+    ░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒░░▒▒▒▒▒▒░░░░
+  ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+                        // github.com/mrcsmxms
+"
 sudo apt-get update
 sudo apt-get install npm -y
 sudo npm -g n
+clear
+echo "Node Package Manager wurde installiert"
 sudo n latest
+clear
+echo "aktuellste Version von Node wurde installiert"
 sudo apt-get install libavahi-compat-libdnssd-dev -y
 sudo npm install -g --unsafe-perm homebridge
-sudo homebridge &
+homebridge &
 sleep 9
 sudo killall homebridge
 cd .homebridge/
 sudo wget https://raw.githubusercontent.com/mrcsmxms/homebridge_installation/master/config.json
 cd
-sudo rm install_homebridge.sh
+clear
+echo "Installation abgeschlossen"
+echo "Homebridge kann jetzt mit dem Befehl 'homebridge' gestartet werden"

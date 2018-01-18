f7ddbd5e6b1108eed5890f5352e323df20611bbd
mrcsmxms <marcus.assenheimer@gmx.de>
2018-01-18 01:12:41 +0100

    abc

--- installHB.sh
+++ installHB.sh
@@ -19,22 +19,29 @@ sudo apt-get update
 sudo apt-get install npm -y
 sudo npm -g n
 clear
-echo "Node Package Manager wurde installiert"
+echo "//======================================\\"
+echo "||Node Package Manager wurde installiert||"
+echo "\\======================================//"
 sudo n latest
 clear
 echo "aktuellste Version von Node wurde installiert"
 sudo apt-get install libavahi-compat-libdnssd-dev -y
 sudo npm install -g --unsafe-perm homebridge
 homebridge &
-sleep 9
+sleep 6
 sudo killall homebridge
+clear
+homebridge &
+sleep 6
+sudo killall homebridge
+clear
 cd .homebridge/
 sudo wget https://raw.githubusercontent.com/mrcsmxms/homebridge_installation/master/config.json
 cd
-<<<<<<< HEAD
+sleep 10
 clear
-echo "Installation abgeschlossen"
-echo "Homebridge kann jetzt mit dem Befehl 'homebridge' gestartet werden"
-=======
-sudo rm installHB.sh
->>>>>>> 233fd82e4201af02058c14899b163426d94bda80
+
+echo "//==================================================================\\"
+echo "||                    Installation abgeschlossen                    ||"
+echo "||Homebridge kann jetzt mit dem Befehl 'homebridge' gestartet werden||"
+echo "\\==================================================================//"

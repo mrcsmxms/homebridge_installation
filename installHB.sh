6446b4d3092225af75771e468572df749e1b9e19
mrcsmxms <marcus.assenheimer@gmx.de>
2018-01-18 01:34:46 +0100

    abc

--- installHB.sh
+++ installHB.sh
@@ -17,29 +17,29 @@ echo "
 sleep 3
 sudo apt-get update
 sudo apt-get install npm -y
-sudo npm -g n
-clear
+sudo npm install -g n
+#clear
 echo "//======================================\\"
 echo "||Node Package Manager wurde installiert||"
 echo "\\======================================//"
 sudo n latest
-clear
+#clear
 echo "aktuellste Version von Node wurde installiert"
 sudo apt-get install libavahi-compat-libdnssd-dev -y
 sudo npm install -g --unsafe-perm homebridge
-homebridge &
-sleep 6
-sudo killall homebridge
-clear
-homebridge &
-sleep 6
-sudo killall homebridge
-clear
-cd .homebridge/
-sudo wget https://raw.githubusercontent.com/mrcsmxms/homebridge_installation/master/config.json
-cd
-sleep 10
-clear
+#homebridge &
+#sleep 6
+#sudo killall homebridge
+#clear
+#homebridge &
+#sleep 6
+#sudo killall homebridge
+#clear
+#cd .homebridge/
+#sudo wget https://raw.githubusercontent.com/mrcsmxms/homebridge_installation/master/config.json
+#cd
+#sleep 10
+#clear
 
 echo "//==================================================================\\"
 echo "||                    Installation abgeschlossen                    ||"
